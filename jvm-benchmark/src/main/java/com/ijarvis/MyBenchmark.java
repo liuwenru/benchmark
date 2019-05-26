1b74956e22f013cb2ddd0a7e75fda9da2e3ebc1b
ijarvis <ijarvis@sina.com>
2019-05-26 14:57:35 +0800

    fix bug

---
+++ jvm-benchmark/src/main/java/com/ijarvis/MyBenchmark.java
@@ -0,0 +1,16 @@
+/*
+ * JVM中对于JIT的压力测试
+ */
+
+package com.ijarvis;
+
+import org.openjdk.jmh.annotations.Benchmark;
+
+public class MyBenchmark {
+
+    @Benchmark
+    public void testMethod() {
+        // place your benchmarked code here
+    }
+
+}
